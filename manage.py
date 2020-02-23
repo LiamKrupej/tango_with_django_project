0c9fb524e68e6882003db92b4d7afffd343b1d04
Liam Krupej <liam.krupej@student.gla.ac.uk>
2020-02-23 22:34:54 +0000

    Chapter 3 (Completed and Tested)

--- manage.py
+++ manage.py
@@ -2,34 +2,15 @@
 import os
 import sys
 
-# if __name__ == '__main__':
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
-#     try:
-#         from django.core.management import execute_from_command_line
-#     except ImportError as exc:
-#         raise ImportError(
-#             "Couldn't import Django. Are you sure it's installed and "
-#             "available on your PYTHONPATH environment variable? Did you "
-#             "forget to activate a virtual environment?"
-#         ) from exc
-#     execute_from_command_line(sys.argv)
-
-#
-# Tango with Django 2 Progress Tests
-# By Leif Azzopardi and David Maxwell
-# With assistance from Enzo Roiz (https://github.com/enzoroiz)
-#
-# Chapter 3 -- Django Basics
-# Last updated October 3rd, 2019
-# Revising Author: David Maxwell
-#
-
-#
-# In order to run these tests, copy this module to your tango_with_django_project/rango/ directory.
-# Once this is complete, run $ python manage.py test rango.tests_chapter3
-#
-# The tests will then be run, and the output displayed -- do you pass them all?
-#
-# Once you are done with the tests, delete the module. You don't need to put it in your Git repository!
-#
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
