e1e59a378d354f058fde3dd5f6715878534d2fb5
Liam Krupej <liam.krupej@student.gla.ac.uk>
2020-02-25 14:26:30 +0000

    Completed Chapter 7 (all tests working)

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,7 @@ from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length= 100, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField()
