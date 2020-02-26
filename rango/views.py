d4859b91064f986b070592562fafd3348cac2921
Liam Krupej <liam.krupej@student.gla.ac.uk>
2020-02-26 14:58:59 +0000

    Chapter 10 (mostly done - could not find unit tests)

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from rango.models import Category
 from rango.models import Page
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 
 
 def index(request):
@@ -16,13 +17,18 @@ def index(request):
 
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list,
                     'pages': page_list}
-
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 def about(request):
     print(request.method)
     print(request.user)
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html', {})
 
 
@@ -152,3 +158,26 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
